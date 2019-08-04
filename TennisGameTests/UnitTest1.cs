9c4cf52c61a58e0937fad61a137965cdb90144b0
Jacopo Bufalino <jacopo.bufalino@aalto.fi>
2019-08-04 15:06:44 +0300

    Add test to assert players are still playing

--- TennisGameTests/UnitTest1.cs
+++ TennisGameTests/UnitTest1.cs
@@ -35,5 +35,20 @@ namespace Tests
             Assert.AreEqual(game.getWinner(), "player2");
 
         }
+
+        [Test]
+        public void TestDeuce()
+        {
+            Game game = new Game();
+            game.addPoint("player2");
+            game.addPoint("player2");
+            game.addPoint("player2");
+            game.addPoint("player1");
+            game.addPoint("player1");
+            game.addPoint("player1");
+
+            Assert.AreEqual(game.getWinner(), "still playing");
+
+        }
     }
 }
